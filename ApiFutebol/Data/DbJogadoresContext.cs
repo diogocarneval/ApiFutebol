20fb50089edf93fe0f7f78255c72dabbf17e43d9
Diogo Carneval <diogosiqueira059@gmail.com>
2024-09-13 09:51:12 -0300

    Criação do Seed do banco de dados

--- ApiFutebol/Data/DbJogadoresContext.cs
+++ ApiFutebol/Data/DbJogadoresContext.cs
@@ -15,5 +15,22 @@ namespace ApiFutebol.Data
         }
 
         public DbSet<ApiFutebol.Models.JogadorFutebol> JogadorFutebol { get; set; } = default!;
+
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<JogadorFutebol>().HasData(
+            new JogadorFutebol
+            {
+                Id = 1,
+                Idade = 30,
+                NumeroCamisa = 21,
+                Nome = "Paulo Dybala",
+                Time = "Roma",
+                Posicao = "Atacante",
+                Nacionalidade = "Argentino",
+                Status = "Ativo"
+            });
+                }
     }
 }
